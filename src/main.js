d93d4d730f1ab2fc59029d1eb75d804b6c51e3ea
dancru89 <danfcruz2@misena.edu.co>
2022-07-06 14:00:20 -0500

    proyecto final laravel front dancru89 para profe alexandra

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vueaxios from "vue-axios"
+import Axios from "axios"
+import axios from "axios"
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+Vue.use(IconsPlugin)
+
+
+
+Vue.use(Vueaxios, axios)
+Vue.use(Vueaxios, Axios)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Productos/Mostrar.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <h1>MOSTRAR PRODUCTOS</h1>
+    
+    <b-table :items="productos"></b-table>
+</div>
+</template>
+
+
+<script>
+import  axios  from 'axios'
+export default{
+    name:"MostrarProductos",
+    data(){
+        return{
+            productos:[],
+        }
+
+    },
+    components:{
+
+    },
+    mounted(){
+
+    },
+    methods:{
+        getproductos(){
+            this.axios.get("http://127.0.0.1:8000/api/productos").then((response)=>{
+                this.productos=response.data;
+            })
+        }
+
+    }
+}
+</script>
+
+
+
+
+<style>
+</style>
